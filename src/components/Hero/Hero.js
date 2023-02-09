b508b599c87fa0441ce6f0f62bcf4c170b6730fa
Andre <33551553+adspinto@users.noreply.github.com>
2023-02-09 15:51:25 -0300

    add assets to docs

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,6 +7,7 @@ function Hero({alignment, mode, readability, image, video, options, height}) {
     const [hover, setHover] = useState(false);
     const handleMouseEnter = () => setHover(true);
     const handleMouseLeave = () => setHover(false);
+
     useEffect(()=> { setShow(true) }, [])
 
     const heroClassname = `hero ${show ? 'hero--show' : ''} ${mode === 'dark' ? 'hero--dark' : 'hero--light'}`
